3ecb338dd7761600afbec4df70ad9de87033be43
alexzarp <alexszds@gmail.com>
2020-11-11 21:38:59 -0300

    Vali-lang

---
+++ Fonte/Variaveis.java
@@ -0,0 +1,13 @@
+package Fonte;
+
+public class Variaveis {
+    public String nome;
+    public String tipo;
+    public double valorFlutuante;
+    public int valorInteiro;
+    public String valorString;
+
+    public Variaveis () {
+        
+    }
+}
