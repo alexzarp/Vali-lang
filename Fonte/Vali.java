a0ddc47413431625f50b6e624f643475a69a02b7
alexzarp <alexszds@gmail.com>
2020-11-18 20:22:55 -0300

    Alt

--- Fonte/Vali.java
+++ Fonte/Vali.java
@@ -1,7 +1,32 @@
-package Fonte;
+import java.io.File;
+import java.util.Scanner;
+public class Vali {
+    public static void main(String[] args)
+    {
+        try {
+            String[] lines = new String[2000];
+            int amount = 0;
+            File file = new File(args[0]);
+            Scanner input = new Scanner(file);
 
-class Vali {
-    public static void main (String[] args) {
+            while (input.hasNextLine())
+            {
+                String line = input.nextLine();
+                lines[amount++] = line;
+            }
 
+            input.close();
+
+            for (int i = 0; i < amount; i++)
+            {
+                System.out.println("Linha " + i + ": " + lines[i]);
+            }
+            
+        } catch (Exception e)
+        {
+            System.out.println("Nao foi possivel abrir o arquivo.vali.");
+            System.out.println("Ele existe mesmo?");
+            e.printStackTrace();
+        }
     }
 }
