e355c9bb353aa6ee422f42cd919d176a9ddae0d4
Rafael Fernandes <51202489+rflgf@users.noreply.github.com>
2020-11-19 13:04:04 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String input = "1+3;";
-        System.out.println("Avaliando "+ input + ":");
-        ExperimentoParser parser = new ExperimentoParser(input, 0);
-        System.out.println(parser.exprecaoAlgebricaEntreInteiros());
-    }
-  }
-  
