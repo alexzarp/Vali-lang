428dd09af3377078300e6aa5b581bfaf60f181ff
Rafael Fernandes <51202489+rflgf@users.noreply.github.com>
2020-11-19 01:42:13 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "1+3";
+        String input = "1+3;";
         ExperimentoParser parser = new ExperimentoParser(input, 0);
         System.out.println(parser.exprecaoAlgebricaEntreInteiros());
     }
