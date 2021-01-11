7d88343834d8df755f8e88225ecf2e886c0c12f2
alexzarp <alexszds@gmail.com>
2021-01-10 22:10:19 -0300

    resolveEscopo tirado fora do parser e tornado público

---
+++ Fonte/Escopo.java
@@ -0,0 +1,44 @@
+public class Escopo {
+    // resolve qualquer atribuição, com expressão algébrica e terminada em ;
+    public void resolveCorpo() throws Erro {
+
+        // utilizamos expressões regulares para várias verificações ao longo da função.
+        Matcher comparador;
+        ignoraWhiteSpace();
+
+        // enquanto ainda há linhas do código fonte a serem avaliadas, verificamos se a próxima linha
+        // se encaixa em algum dos contextos esperados (chamadas de funções, atribuições de funções,
+        // loops, condicionais e atribuições de variáveis). se a linha não se encaixar em nenhum contexto,
+        // jogamos um erro de token inesperado.
+        while(indiceAbsoluto <= comprimentoDoPrograma - 2) { // -1 por causa do índice 0 e -1 por causa do \n extra que adicionamos no Vali.java
+        ignoraWhiteSpace();
+            // regex para algo no formato "palavra palavra(" ou "palavra("
+            comparador = Pattern.compile("(\\s*\\w\\s)?(\\w*\\s\\()").matcher(codigoFonte);
+
+            // se entrar, é porque encontrou uma chamada ou definição de função (loops
+            // inclusive)
+            if(comparador.find() && comparador.start(indiceAbsoluto) == 0) {
+
+                // verificaSe();
+                // verificaEnquanto();
+                // verificaPara();
+                // verificaImprime();
+
+                // verificaAtribuicaoFuncao();
+
+                // ps: lembrar de usar novoEscopo() e removeEscopo() toda vez que chamar uma
+                // função, seja um loop, if, ou função declarada no código.
+
+            } else {
+
+                // como não é nenhuma função, resta apenas testar se há uma atribuição.
+                if(!verificaAtribuicaoVariavel()) {
+                    // como a linha não se adequa a nenhum dos contextos possíveis, apenas dizemos que o token é inesperado.
+                    throw new TokenInesperado(codigoFonte, indiceAbsoluto);
+                }
+                    
+            }
+        }
+        System.out.println("execução com sucesso do programa.");
+    }
+}
--- Fonte/Parser.java
+++ Fonte/Parser.java
@@ -22,6 +22,8 @@ import java.util.regex.Matcher;
     - "a{n,m}" significa "a" de n à m ocorrências seguidas */
 
 public class Parser {
+    // instancia das classes externas
+    Escopo escopo = new Escopo();
 
     // codigoFonte guarda todo o código fonte em uma única string.
     public String codigoFonte;
@@ -40,50 +42,7 @@ public class Parser {
 
     public void analisa() throws Erro {
         Variavel.novoEscopo();
-        resolveCorpo();
-    }
-
-    // resolve qualquer atribuição, com expressão algébrica e terminada em ;
-    private void resolveCorpo() throws Erro {
-
-        // utilizamos expressões regulares para várias verificações ao longo da função.
-        Matcher comparador;
-        ignoraWhiteSpace();
-
-        // enquanto ainda há linhas do código fonte a serem avaliadas, verificamos se a próxima linha
-        // se encaixa em algum dos contextos esperados (chamadas de funções, atribuições de funções,
-        // loops, condicionais e atribuições de variáveis). se a linha não se encaixar em nenhum contexto,
-        // jogamos um erro de token inesperado.
-        while(indiceAbsoluto <= comprimentoDoPrograma - 2) { // -1 por causa do índice 0 e -1 por causa do \n extra que adicionamos no Vali.java
-        ignoraWhiteSpace();
-            // regex para algo no formato "palavra palavra(" ou "palavra("
-            comparador = Pattern.compile("(\\s*\\w\\s)?(\\w*\\s\\()").matcher(codigoFonte);
-
-            // se entrar, é porque encontrou uma chamada ou definição de função (loops
-            // inclusive)
-            if(comparador.find() && comparador.start(indiceAbsoluto) == 0) {
-
-                // verificaSe();
-                // verificaEnquanto();
-                // verificaPara();
-                // verificaImprime();
-
-                // verificaAtribuicaoFuncao();
-
-                // ps: lembrar de usar novoEscopo() e removeEscopo() toda vez que chamar uma
-                // função, seja um loop, if, ou função declarada no código.
-
-            } else {
-
-                // como não é nenhuma função, resta apenas testar se há uma atribuição.
-                if(!verificaAtribuicaoVariavel()) {
-                    // como a linha não se adequa a nenhum dos contextos possíveis, apenas dizemos que o token é inesperado.
-                    throw new TokenInesperado(codigoFonte, indiceAbsoluto);
-                }
-                    
-            }
-        }
-        System.out.println("execução com sucesso do programa.");
+        escopo.resolveCorpo();
     }
 
     // TODO implementar isso.
